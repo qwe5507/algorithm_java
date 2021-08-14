4d1cce4dc2828948438e86b82ee17e5ca72fcdc2
qwe5507 <apvmffkdls@naver.com>
2021-08-14 11:09:32 +0900

    퀴즈2 - 비밀번호 형식 검사

---
+++ src/quiz1/Code2.java
@@ -0,0 +1,22 @@
+package quiz1;
+
+public class Code2 {
+    private static boolean checkPassword(String s) {
+        if(s.length() < 5){
+            return false;
+        }
+        if( !(s.matches(".*[0-9].*"))) {
+            return false;
+        }
+        if( !(s.matches(".*[A-Z].*"))) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkPassword("8eqq") == false);
+        System.out.println(checkPassword("password1") == false);
+        System.out.println(checkPassword("Password1") == true);
+    }
+}
