def2c00bc26387cb5a08b2f09fb85f84cd936ce5
qwe5507 <apvmffkdls@naver.com>
2021-10-28 00:07:50 +0900

    프로그래머스 - 같은 숫자는 싫어 [자바 풀이]

---
+++ src/programmers/Quiz.java
@@ -0,0 +1,30 @@
+package programmers;
+
+/*
+프로그래머스 - 같은 숫자는 싫어 [자바 풀이]
+ */
+
+import java.util.ArrayList;
+
+public class Quiz {
+    public static class Solution {
+        public int[] solution(int []arr) {
+            ArrayList<Integer> list = new ArrayList();
+            int saveNum = -1;
+
+            for (int i=0; i<arr.length; i++) {
+                if (saveNum != arr[i]) {
+                    saveNum=arr[i];
+                    list.add(arr[i]);
+                }
+            }
+            int[] answer = new int[list.size()];
+
+            for (int i=0; i < list.size(); i++) {
+                answer[i] = list.get(i);
+            }
+
+            return answer;
+        }
+    }
+}
