9d6c30ac1cc8268a25e3967a1767ea1ec9d4cdfb
qwe5507 <apvmffkdls@naver.com>
2022-05-09 18:08:20 +0900

    프로그래머스 1LEVEL - 소수만들기

---
+++ src/programmers/level1/소수만들기.java
@@ -0,0 +1,37 @@
+package programmers.level1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class 소수만들기 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1,2,7,6,4}));
+    }
+
+    public static int solution(int[] nums) {
+        int answer = 0;
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int z = j + 1; z < nums.length; z++) {
+                    boolean flag = false;
+                    int sum = nums[i] + nums[j] + nums[z];
+
+                    for (int h = 2; h < sum-1; h++) {
+                        if (sum % h == 0) {
+                            flag = true;
+                        }
+                    }
+
+                    if (!flag) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
